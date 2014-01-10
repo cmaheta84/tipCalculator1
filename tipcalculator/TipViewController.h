114d71581a553ee904cac6e39ddf7ea549a42e58
psandip <psandip@yahoo-inc.com>
2014-01-09 20:19:56 -0800

    calculator files

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Sandip Patel on 1/9/14.
+//  Copyright (c) 2014 Y.CORP.YAHOO.COM\psandip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
